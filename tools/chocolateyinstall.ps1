e09431376b2d3e069bfd6119d34e22e27c282afc
Jian Liu <jianliu@mbp15.local>
2020-06-14 12:24:58 -0700

    First commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+$filesDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" -replace "tools", "files"
+
+if ([Environment]::OSVersion.version.major -ge 5 -and [Environment]::Is64BitOperatingSystem)
+{
+  robocopy "$filesDir" "C:\Windows\" /s /XX
+  sc.exe create npf binPath=system32\drivers\npf.sys type=kernel start=auto error=normal tag=no DisplayName="WinPcap Packet Filter Driver"
+  sc.exe start npf
+}
+else 
+{
+  throw "This package only supports Vista+ and 64bit Windows OS"
+}
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'
+$filesDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" -replace "tools", "files"
+gci -file "$filesDir\*.*" -r | select -expandproperty fullname | foreach {
+  $path = $_ -replace [regex]::escape($filesDir), "C:\Windows"
+  if (test-path "$path") {
+    remove-item "$path" -force
+  }
+}
